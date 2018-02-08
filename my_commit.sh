e4f58ce18af3b288bdd3d3c59c290dc51f5940c5
Alejandro Villamarin <favm@email.com>
2018-02-08 17:30:28 +0100

    Create my_commit.sh
    #For smart commits:
    #./mycommit.sh <jira ticket> "Deleted debugging prints."

---
+++ my_commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+comment=$1
+comment="$1 #comment $2"
+echo $comment
+git add .
+git commit --author="alexandrosv <alex@mail.com>" -m "$comment"
+git push
+
+#For smart commits:
+#./mycommit.sh <jira ticket> "Deleted debugging prints."
+~
